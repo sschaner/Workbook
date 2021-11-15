167f011c80559526bf3bf2d5246edd16e83b478f
Steve Schaner <schanerst@gmail.com>
2021-11-15 08:51:33 -0500

    Exercise 49 complete.

---
+++ Exercise49/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise49
+{
+    class Program
+    {
+        /*
+        * Exercise 49
+        * 
+        * Description:
+        * Create a list of squares.
+        * Use the Square class.
+        * Display some statistics for the collection of squares.
+        */
+        static void Main(string[] args)
+        {
+            Console.Title = "Exercise 49";
+
+            string userInput = "";
+            bool enterAgain = true;
+            do
+            {
+                bool continueEnteringSideLength = true;
+                List<int> squaresList = new List<int>();
+                AddToSquaresList(userInput, squaresList, continueEnteringSideLength);
+
+                Square square = new Square();
+
+                double sumOfAreas = 0;
+                double sumOfPerimeters = 0;
+                for (int i = 0; i < squaresList.Count; i++)
+                {
+                    sumOfAreas = sumOfAreas + square.AreaOfSquare(squaresList[i]);
+                    Math.Round(sumOfAreas, 2);
+                    sumOfPerimeters = sumOfPerimeters + square.PerimeterOfSquare(squaresList[i]);
+                }
+
+                double averageArea = Math.Round((sumOfAreas / squaresList.Count()), 2);
+                double averagePerimeter = Math.Round((sumOfPerimeters / squaresList.Count()), 2);
+
+                Console.WriteLine($"You created {squaresList.Count()} squares.");
+                Console.WriteLine($"Largest: {squaresList.Max()}");
+                Console.WriteLine($"Smallest: {squaresList.Min()}");
+                Console.WriteLine($"Average Area: {averageArea}");
+                Console.WriteLine($"Average Perimeter: {averagePerimeter}");
+
+                string continueInput = "";
+                do // Loop for determining if the user wants to enter text again
+                {
+                    // Ask the user if they would like to continue
+                    Console.Write("\nWould you like to continue (y/n)? ");
+                    userInput = Console.ReadLine();
+                    continueInput = userInput;
+                    if (continueInput.ToLower().Trim() == "y")
+                    {
+                        enterAgain = true;
+                    }
+                    else if (continueInput.ToLower().Trim() == "n")
+                    {
+                        Console.WriteLine("Goodbye!");
+                        enterAgain = false;
+                        goto Exit;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter either 'y' or 'n'");
+                    }
+                } while (continueInput.ToLower().Trim() != "y");
+            } while (enterAgain == true);
+            
+            Exit:
+
+            Console.ReadKey();
+        }
+
+        public static void AddToSquaresList (string userInput, List<int> squaresList, bool continueEnteringSideLength)
+        {
+            do
+            {
+                int sideLength = -1;
+                Console.Write("Enter a side length (q to quit): ");
+                userInput = Console.ReadLine();
+                bool isValidNumber = int.TryParse(userInput, out sideLength);
+                if (isValidNumber == true)
+                {
+                    squaresList.Add(sideLength);
+                }
+
+                if (userInput.ToLower().Trim() == "q")
+                {
+                    continueEnteringSideLength = false;
+                }
+            } while (continueEnteringSideLength == true);
+        }
+
+    }
+}
---
+++ Exercise49/Square.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise49
+{
+    /*
+     * Create a list of squares.
+     * Use the Square class you built in Lab 9.
+     * Then display some statistics for the collection of squares
+     */
+    public class Square
+    {
+        private int sideLength { get; set; }
+        public int SideLength
+        {
+            get { return sideLength; }
+            set { sideLength = SideLength; }
+        }
+
+        public int AreaOfSquare(int sideLength)
+        {
+            int area = sideLength * sideLength;
+            return area;
+        }
+
+        public int PerimeterOfSquare(int sideLength)
+        {
+            int perimeter = 4 * sideLength;
+            return perimeter;
+        }
+
+
+    }
+}
