b6faa5835c11f392d639f8a383035bb6388cf2d4
Steve Schaner <schanerst@gmail.com>
2021-11-30 12:45:17 -0500

    Exercise 43 complete.

---
+++ Exercise43/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise43
+{
+    public class Point
+    {
+        private int x;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        private int y;
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double distanceFromOrigin (int x, int y)
+        {
+            double distanceFromOrigin = 0.0d;
+
+            distanceFromOrigin = Math.Sqrt(Math.Pow((x - 0), 2) + Math.Pow((y - 0), 2));
+
+            return distanceFromOrigin;
+        }
+    }
+}
---
+++ Exercise43/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Exercise43
+{
+    class Program
+    {
+        /*
+        * Exercise 43
+        * Description:
+        * Enhance the point class in the previous exercise by adding a CalculateDistance method.
+        * The distance method will calculate the distance of a point from the origin, (0,0).
+        */
+        static void Main(string[] args)
+        {
+            Console.Title = "Exercise 43";
+
+            bool enterAgain = true;
+
+            do
+            {
+                Console.Write("Enter an X coordinate: ");
+                int xCoordinate = int.Parse(Console.ReadLine());
+                Console.Write("Enter a Y coordinate: ");
+                int yCoordinate = int.Parse(Console.ReadLine());
+
+                Point point = new Point(xCoordinate, yCoordinate);
+
+                Console.WriteLine($"You have created a point object ({point.X},{point.Y}). It has a distance of {point.distanceFromOrigin(xCoordinate, yCoordinate)}.");
+
+                string continueInput = "";
+                do // Loop for determining if the user wants to enter text again
+                {
+                    // Ask the user if they would like to continue
+                    Console.Write("\nWould you like to continue (y/n)? ");
+                    string userInput = Console.ReadLine();
+                    continueInput = userInput;
+                    if (continueInput.ToLower().Trim() == "y")
+                    {
+                        enterAgain = true;
+                    }
+                    else if (continueInput.ToLower().Trim() == "n")
+                    {
+                        Console.WriteLine("Goodbye!");
+                        enterAgain = false;
+                        goto Exit;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter either 'y' or 'n'");
+                    }
+                } while (continueInput.ToLower().Trim() != "y");
+            } while (enterAgain == true);
+
+        Exit:
+
+            Console.ReadKey();
+        }
+    }
+}
