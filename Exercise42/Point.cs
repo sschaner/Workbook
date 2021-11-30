0dc2eb9a1d9b7b2191f89680158b6349d6a791c5
Steve Schaner <schanerst@gmail.com>
2021-11-30 12:30:14 -0500

    Exercise 42 complete.

---
+++ Exercise42/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise42
+{
+    public class Point
+    {
+        private int x;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        private int y;
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Exercise42/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Exercise42
+{
+    class Program
+    {
+        /*
+        * Exercise 42
+        * Description:
+        * Create a class called Point, that has two properties, X and Y.
+        * Let the user enter an X and Y value to create a Point object.
+        */
+        static void Main(string[] args)
+        {
+            Console.Title = "Exercise 42";
+
+            bool enterAgain = true;
+
+            do
+            {
+                Console.Write("Enter an X coordinate: ");
+                int xCoordinate = int.Parse(Console.ReadLine());
+                Console.Write("Enter a Y coordinate: ");
+                int yCoordinate = int.Parse(Console.ReadLine());
+
+                Point point = new Point(xCoordinate, yCoordinate);
+
+                Console.WriteLine($"You have created a point object ({point.X},{point.Y}).");
+
+                string continueInput = "";
+                do // Loop for determining if the user wants to enter text again
+                {
+                    // Ask the user if they would like to continue
+                    Console.Write("\nWould you like to continue (y/n)? ");
+                    string userInput = Console.ReadLine();
+                    continueInput = userInput;
+                    if (continueInput.ToLower().Trim() == "y")
+                    {
+                        enterAgain = true;
+                    }
+                    else if (continueInput.ToLower().Trim() == "n")
+                    {
+                        Console.WriteLine("Goodbye!");
+                        enterAgain = false;
+                        goto Exit;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter either 'y' or 'n'");
+                    }
+                } while (continueInput.ToLower().Trim() != "y");
+            } while (enterAgain == true);
+
+        Exit:
+
+            Console.ReadKey();
+        }
+    }
+}
