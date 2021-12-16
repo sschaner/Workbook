708e4cc5f5525f864fb1700da01235ec3063f0e1
Steve Schaner <schanerst@gmail.com>
2021-12-16 12:45:21 -0500

    Exercise 58 complete, practice creating enums.

---
+++ Exercise58/Enums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise58
+{
+    enum Suit
+    {
+        Diamonds = 1,
+        Hearts,
+        Clubs,
+        Spades
+    }
+
+    enum Browser
+    {
+        Chrome = 1,
+        Firefox,
+        IE,
+        Opera
+    }
+
+    enum Brand
+    {
+        Polo = 1,
+        Gucci,
+        Armani,
+        Athleta
+    }
+
+    enum Day
+    {
+        Sunday = 1,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ Exercise58/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title();
+
+            Exit();
+        }
+
+        public static void Title()
+        {
+            Console.Title = "Exercise 58";
+        }
+
+        public static void Exit()
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
